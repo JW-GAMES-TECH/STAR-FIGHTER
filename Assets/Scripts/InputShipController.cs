551d55fad5da7da488ee0a1f3ced064643da6eea
JW-GAMES-TECH <94858535+JW-GAMES-TECH@users.noreply.github.com>
2024-12-07 20:24:30 +0000

    Script Update

--- Assets/Scripts/InputShipController.cs
+++ Assets/Scripts/InputShipController.cs
@@ -4,41 +4,48 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-
+[RequireComponent(typeof(Rigidbody))]
 
 public class InputShipController : MonoBehaviour
 {
     [Header("=== Ship Movement Settings ===")]
-    public float flightSpeed = 50;
-    public float rotateSpeed = 20;
-    public float upDownSpeed = 10;
-    public float turningSpeed = 10;
-    public float boostSpeed = 80;
+    [SerializeField]
+    private float yawTorque = 500f;
+    [SerializeField]
+    private float pitchTorque = 1000f;
+    [SerializeField]
+    private float rollTorque = 1000f;
+    [SerializeField]
+    private float thrust = 100f;
+    [SerializeField]
+    private float upThrust = 50f;
+    [SerializeField]
+    private float strafeThrust = 50f;
 
-    public Rigidbody rb;
+    [Header("===Boost Settings ====")]
 
+
+    [SerializeField, Range(0.001f, 0.999f)]
+    private float thrustGlideReduction = 0.999f;
+    [SerializeField, Range(0.001f, 0.999f)]
+    private float upDownGlideReduction = 0.111f;
+    [SerializeField, Range(0.001f, 0.999f)]
+    private float leftRightGlideReduction = 0.111f;
+    float glide, verticalGlide, horizontalGlide = 0f;
+    
+    
 
     //Input Values
-    private float Move;
-    private float Turn;
+    private float thrust1D;
+    private float upDown1D;
+    private float strafe1D;
+    private float roll1D;
+    private Vector2 pitchYaw;
+
+    
 
 
-    //ShipController shipScript;
-    private Vector2 _input;
-
-
-    [System.Serializable]
-    public class InputSettings
-    {
-        public string FowardInput = "Vertical";
-        public string strafeInput = "Horizontal";
-        public string boostInput = "Boost";
-
-    }
-
-
-
-
+    public Rigidbody rb;
 
 
     private void Awake()
@@ -62,29 +69,104 @@ public class InputShipController : MonoBehaviour
 
     }
 
-   void HandleMovement()
+    void HandleMovement()
     {
+        //Roll
+        rb.AddRelativeTorque(Vector3.back * roll1D * rollTorque * Time.deltaTime);
+        //Pitch
+        rb.AddRelativeTorque(Vector3.right * Mathf.Clamp(-pitchYaw.y, -1f, 1f) * pitchTorque * Time.deltaTime);
+        //Yaw
+        rb.AddRelativeTorque(Vector3.up * Mathf.Clamp(pitchYaw.x, -1f, 1f) * yawTorque * Time.deltaTime);
+
+        //Thrust
+        if (thrust1D > 0.1f || thrust1D < -0.1f)
+        {
+            float currentThrust = thrust;
+            rb.AddRelativeForce(Vector3.forward * thrust1D * currentThrust * Time.deltaTime);
+            glide = thrust;
+        }
+
+        else
+        {
+            rb.AddRelativeForce(Vector3.forward * glide * Time.deltaTime);
+            glide *= thrustGlideReduction;
+        }
+        //Up and Down 
+
+        if (upDown1D > 0.1f || upDown1D < -0.1f)
+        {
+
+            rb.AddRelativeForce(Vector3.up * upDown1D * upThrust * Time.fixedDeltaTime);
+            verticalGlide = upDown1D * upThrust;
+        }
+
+        else
+        {
+            rb.AddRelativeForce(Vector3.up * verticalGlide * Time.fixedDeltaTime);
+            verticalGlide *= upDownGlideReduction;
+        }
+
+        //Strafe
+        if (strafe1D > 0.1f || strafe1D < -0.1f)
+        {
+
+            rb.AddRelativeForce(Vector3.right * strafe1D * upThrust * Time.fixedDeltaTime);
+            horizontalGlide = strafe1D * strafeThrust;
+
+           
+
+        }
+
+        else
+        {
+            rb.AddRelativeForce(Vector3.right * horizontalGlide * Time.fixedDeltaTime);
+            horizontalGlide *= leftRightGlideReduction;
+        }
+
+    }
+    
+       
+
+
+
+
+
+
+        
+
+
+    #region Input Methods
+
+    public void OnThrust(InputAction.CallbackContext context)
+    {
+
+
+        thrust1D = context.ReadValue<float>();
 
 
 
     }
 
-
-    public void OnMove(InputAction.CallbackContext context)
+    public void OnStrafe(InputAction.CallbackContext context)
     {
-        //_input = context.ReadValue<Vector2>();
-        //Debug.Log(_input);
-
-        Move = context.ReadValue<float>();
-
-
-
+        strafe1D = context.ReadValue<float>();
     }
 
-    public void OnTurn(InputAction.CallbackContext context)
+    public void OnupDown(InputAction.CallbackContext context)
     {
-        Turn = context.ReadValue<float>();
+        upDown1D = context.ReadValue<float>();
     }
+
+    public void OnRoll(InputAction.CallbackContext context)
+    {
+        roll1D = context.ReadValue<float>();
+    }
+
+    public void OnPitchYaw(InputAction.CallbackContext context)
+    {
+        pitchYaw = context.ReadValue<Vector2>();
+    }
+
 
 
 
@@ -111,6 +193,8 @@ public class InputShipController : MonoBehaviour
 
 
 
+
+    #endregion
 
     #endregion
 
